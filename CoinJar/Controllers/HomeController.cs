310a0f15b18ed9c62473b6894228921a0ef7c7c8
Parham1381 <Parham1381@yahoo.com>
2015-07-27 07:26:20 -0700

    Coin Jar in C#
    Please notice that the project is created by ASP.NET MVC 4.0

---
+++ CoinJar/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Configuration;
+using CoinJar.Models;
+
+namespace CoinJar.Controllers
+{
+
+
+    /// <summary>
+    /// Home Controller is the only main controller of this application
+    /// </summary>
+    public class HomeController : Controller 
+    {
+        /// <summary>
+        /// List of US Coins 
+        /// </summary>
+        public IEnumerable<USCoin> USCoinList
+        {
+            get
+            {
+                yield return new Cent();
+                yield return new Nickel(); 
+                yield return new Dime();
+                yield return new Quarter();
+                yield return new HalfDollar();
+                yield return new Dollar();
+            }
+        }
+
+        /// <summary>
+        /// The main action result of application
+        /// </summary>
+        /// <returns>Return ActionResult as output value</returns>
+        public ActionResult Index()
+        {
+            //Declare a US coin Jar
+            USCoinJar usCoinJar = new USCoinJar();
+            //Define an inline function to add multiple of coins
+            Func<USCoin, byte, USCoinJar> AddCoins = (usCoin , count) =>
+                {
+                    for (byte i = 0; i < count ; i ++)
+                        usCoinJar.Add (usCoin);
+                    return usCoinJar;
+                };
+
+            //Add coins to the list of Coins in Jar
+            AddCoins ( new Dollar (), 20);
+            AddCoins(new HalfDollar(), 10);
+            AddCoins(new Quarter(), 45);
+            AddCoins(new Dime(), 15);
+            AddCoins(new Cent(), 25);
+
+            //Preparing ViewBag to render the view
+            ViewBag.USCoinList = USCoinList;
+            ViewBag.USCoinJar = usCoinJar;
+
+            return View();
+        }
+        
+    }
+}
---
+++ CoinJar/Models/CoinJar.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CoinJar.Models
+{
+    /// <summary>
+    /// public abstract generic class Coin Jar. 
+    /// </summary>
+    /// <typeparam name="T">Type of Currency like USCurrency</typeparam>
+    public abstract class CoinJar<T> where T: Currency
+    {
+        // Maximum Volume that Coin Jar can hold
+        public Volume MaxVolume { get; protected set; } 
+        // Current Amount of Coin Jar.
+        public T CurrentAmount { get; protected set; }
+        // Current Volume of Coin Jar
+        public Volume CurrentVolume { get; protected set; }
+        // List of Coins in Coin Jar
+        public abstract IEnumerable<Coin<T>> Coins { get; }
+
+        // Method to add a new coim
+        public abstract void Add(Coin<T> coin);
+        // Method to reset the Coin Jar
+        public abstract void Reset();
+    }
+
+    /// <summary>
+    /// US Coin jar which implemend actual Coin Jar.
+    /// </summary>
+    public class USCoinJar : CoinJar<USCurrency>
+    {
+        // define list of Coins
+        private List<Coin<USCurrency>> _coins = new List<Coin<USCurrency>>();
+        
+        // List of Coins
+        public override IEnumerable<Coin<USCurrency>> Coins
+        {
+            get { return _coins; }
+        } 
+
+        // Default constructor of class
+        public USCoinJar()
+        {
+            CurrentVolume = new FluidOunces();
+            MaxVolume = new FluidOunces (32);
+            Reset();
+        }
+
+        
+        /// <summary>
+        /// public method of Add coin
+        /// </summary>
+        /// <param name="coin">US coin that needs to be added</param>
+        public override void Add(Coin<USCurrency> coin)
+        {
+            // if volume of Coin Jar is not enough to add a new coin, raise an appropriate exception
+            if ((CurrentVolume.Unit + coin.Volume.Unit) > MaxVolume.Unit)
+                // throw new CoinOverFlowException
+                throw new CoinOverFlowException();
+
+            _coins.Add(coin);
+            CurrentVolume = CurrentVolume + (Volume)coin.Volume;
+            CurrentAmount = CurrentAmount + coin.Price;
+        }
+
+        /// <summary>
+        /// Public method of Reset to release all coins
+        /// </summary>
+        public override void Reset()
+        {
+            _coins = new List<Coin<USCurrency>>();
+            CurrentVolume = new FluidOunces();
+            CurrentAmount = new USCurrency();
+        }
+    }
+
+    /// <summary>
+    /// Class of Coin OverFlow Exception that inherits from Exception
+    /// </summary>
+    public class CoinOverFlowException : Exception
+    {
+        // Set the appropriate Error Message during construction
+        public CoinOverFlowException()
+            : base("Coins overflowed the jar")
+        {
+        }
+    }
+
+}
